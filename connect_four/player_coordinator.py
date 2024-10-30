1da68eda2572c1dd36ed8632980623b7a63ad737
Dario Bauer <dario.bauer@stud.hslu.ch>
2024-10-30 10:47:13 +0100

    SW07: player_coordinater drop state

--- connect_four/player_coordinator.py
+++ connect_four/player_coordinator.py
@@ -17,19 +17,34 @@ class PlayerCoordinator:
         # TODO check if the column is valid (0..6) => return the appropriate DropState
         # TODO check if the column is full => return the appropriate DropState
         # TODO insert token into board (column = column_to_drop)
+        if(column<=-1 or column>=len(self._board[0])):
+            return DropState.COLUMN_INVALID
+        
+        if(self._board[0][column] != GameToken.EMPTY):
+            return DropState.COLUMN_INVALID
+
+        for row in range(len(self._board)-1,-1,-1):
+            if(self._board[row][column] == GameToken.EMPTY):
+                self._board[row][column] = player
+                break
+    
         return DropState.DROP_OK
 
     def run(self):
         # play game until won or draw
         while (True):
-
+            valid = DropState.COLUMN_INVALID
             self._player_red.draw_board(self._board, self._state)
             column_to_drop = self._player_red.play_turn()  # get the move of the player
-            self.drop_token(GameToken.RED, column_to_drop)
+            while(valid != DropState.DROP_OK):
+                valid = self.drop_token(GameToken.RED, column_to_drop)
+            valid = DropState.COLUMN_INVALID
 
             self._player_yellow.draw_board(self._board, self._state)
             column_to_drop = self._player_yellow.play_turn()  # get the move of the player
-            self.drop_token(GameToken.YELLOW, column_to_drop)
+            while(valid != DropState.DROP_OK):
+                valid = self.drop_token(GameToken.YELLOW, column_to_drop)
+            valid = DropState.COLUMN_INVALID
 
 
 # start a local game
