36b2efac6ff0e0b7cab7105362bea29e523125b1
justinmeckmann1 <justin.meckmann@stud.hslu.ch>
2024-11-13 11:22:30 +0100

    update game logic

---
+++ connect_four/game_logic.py
@@ -0,0 +1,35 @@
+from game_logic_base import GameLogicBase
+from player_sensehat import PlayerSenseHat
+from player_console import PlayerConsole
+from game_token import GameToken
+from game_state import *
+from drop_state import DropState
+
+class GameLogic(GameLogicBase):
+    def __init__(self):
+        self._board = [[GameToken.EMPTY for _ in range(7)] for _ in range(6)]
+        self._state = GameState.TURN_RED
+
+    def get_board(): -> list:
+        pass
+
+    def drop_token(self, player: GameToken, column: int) -> DropState:
+        # check if the column is valid (0..6) => return the appropriate DropState
+        if(column<=-1 or column>=len(self._board[0])):
+            return DropState.COLUMN_INVALID
+        
+        # check if the column is full => return the appropriate DropState
+        if(self._board[0][column] != GameToken.EMPTY):
+            return DropState.COLUMN_INVALID
+
+        # insert token into board (column = column_to_drop)
+        for row in range(len(self._board)-1,-1,-1):
+            if(self._board[row][column] == GameToken.EMPTY):
+                self._board[row][column] = player
+                break
+    
+        return DropState.DROP_OK
+
+    def get_state(): -> GameState
+        pass
+        
--- connect_four/player_coordinator.py
+++ connect_four/player_coordinator.py
@@ -18,22 +18,22 @@ class PlayerCoordinator:
         self._board = [[GameToken.EMPTY for _ in range(7)] for _ in range(6)]
         self._state = GameState.TURN_RED
 
-    def drop_token(self, player: GameToken, column: int) -> DropState:
-        # check if the column is valid (0..6) => return the appropriate DropState
-        if(column<=-1 or column>=len(self._board[0])):
-            return DropState.COLUMN_INVALID
+    # def drop_token(self, player: GameToken, column: int) -> DropState:
+    #     # check if the column is valid (0..6) => return the appropriate DropState
+    #     if(column<=-1 or column>=len(self._board[0])):
+    #         return DropState.COLUMN_INVALID
         
-        # check if the column is full => return the appropriate DropState
-        if(self._board[0][column] != GameToken.EMPTY):
-            return DropState.COLUMN_INVALID
+    #     # check if the column is full => return the appropriate DropState
+    #     if(self._board[0][column] != GameToken.EMPTY):
+    #         return DropState.COLUMN_INVALID
 
-        # insert token into board (column = column_to_drop)
-        for row in range(len(self._board)-1,-1,-1):
-            if(self._board[row][column] == GameToken.EMPTY):
-                self._board[row][column] = player
-                break
+    #     # insert token into board (column = column_to_drop)
+    #     for row in range(len(self._board)-1,-1,-1):
+    #         if(self._board[row][column] == GameToken.EMPTY):
+    #             self._board[row][column] = player
+    #             break
     
-        return DropState.DROP_OK
+    #     return DropState.DROP_OK
 
     def run(self):
         # play game until won or draw
