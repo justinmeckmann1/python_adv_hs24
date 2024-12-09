4c0980d18742643bcf378364fe2ef5157e9d301b
justinmeckmann <justin.meckmann@stud.hslu.ch>
2024-12-09 13:41:59 +0100

    added remote player coordinator

--- connect_four/player_coordinator.py
+++ connect_four/player_coordinator.py
@@ -7,51 +7,58 @@ from game_state import *
 from drop_state import DropState
 from game_logic_base import GameLogicBase
 from game_logic import GameLogic
+from game_logic_client import GameLogicClient
+
+color = 'red'
+# color = 'yellow'
 
 class PlayerCoordinator:
     def __init__(self):
         # initialize players
-        if os.name == 'nt':
-            self._player_red = PlayerConsole(GameToken.RED)  # X
-            self._player_yellow = PlayerConsole(GameToken.YELLOW)  # 0
+        if os.name != 'nt':
+            self._player = PlayerSenseHat(GameToken.RED) if color == 'red' else PlayerSenseHat(GameToken.YELLOW)
         else:
-            self._player_red = PlayerConsole(GameToken.RED)  # X
-            self._player_yellow = PlayerSenseHat(GameToken.YELLOW)  # 0
+            self._player = PlayerConsole(GameToken.RED) if color == 'red' else PlayerConsole(GameToken.YELLOW)
 
     def run(self, game: GameLogicBase):
-        self._player_red.draw_board(game.get_board(), game.get_state())
-        self._player_yellow.draw_board(game.get_board(), game.get_state())
+        self._player.draw_board(game.get_board(), game.get_state())
         # play game until won or draw
         while (True):
             # Get current player based on state
-            current_player = (self._player_red if game.get_state() == GameState.TURN_RED 
-                            else self._player_yellow)
-            current_token = (GameToken.RED if current_player == self._player_red 
-                           else GameToken.YELLOW)
+            state = game.get_state()
             
-            # Player's turn
+            current_player = self._player if (
+                (self._player.player_id == GameToken.RED and state == GameState.TURN_RED) or 
+                (self._player.player_id == GameToken.YELLOW and state == GameState.TURN_YELLOW)
+            ) else None
+            
+            current_token = (GameToken.RED if state == GameState.TURN_RED else GameToken.YELLOW)
+            
             valid = DropState.COLUMN_INVALID
-            current_player.draw_board(game.get_board(), game.get_state())
-            while valid != DropState.DROP_OK:
+            
+            self._player.draw_board(game.get_board(), game.get_state())
+            
+            while valid != DropState.DROP_OK and current_player is not None:
                 column_to_drop = current_player.play_turn()
                 valid = game.drop_token(current_token, column_to_drop)
-            current_player.draw_board(game.get_board(), game.get_state())
+            
+            self._player.draw_board(game.get_board(), game.get_state())
 
             # Check if game is over 
             temp_gamestate = game.get_state()
             if temp_gamestate in [GameState.WON_YELLOW, GameState.WON_RED, GameState.DRAW]:
                 winner_token = (GameToken.RED if temp_gamestate == GameState.WON_RED 
                             else GameToken.YELLOW)
-                current_player.display_winner(winner_token)
-                current_player = (self._player_yellow if temp_gamestate == GameState.WON_RED #select the looser
-                            else self._player_red)
-                current_player.draw_board(game.get_board(), temp_gamestate) #draw board for the looser
-                current_player.display_winner(winner_token)
+                self._player.display_winner(winner_token)
+                # current_player = (self._player_yellow if temp_gamestate == GameState.WON_RED #select the looser
+                #             else self._player_red)
+                # current_player.draw_board(game.get_board(), temp_gamestate) #draw board for the looser
+                # current_player.display_winner(winner_token)
                 break
 
 # start a local game
 if __name__ == '__main__':
-    game = GameLogic()
+    game = GameLogicClient(host='localhost:5000')
     coordinator = PlayerCoordinator()
     coordinator.run(game)
 
